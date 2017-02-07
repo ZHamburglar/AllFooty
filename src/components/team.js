ad06877202032643f87ba2f3f530df899c58d70a
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-02-07 09:30:38 -0600

    got the image working

--- src/components/team.js
+++ src/components/team.js
@@ -3,6 +3,7 @@ import FootballData from '../api/football-data';
 import Request from 'superagent';
 import axios from 'axios';
 
+
 export default class Team extends React.Component {
   constructor() {
     super();
@@ -13,19 +14,23 @@ export default class Team extends React.Component {
 
   componentWillMount() {
     // Called the first time the component is loaded right before the component is added to the page
+
   }
 
   componentDidMount() {
+    let teamIdName = this.props.params.teamId
+    console.log('teamIdName', teamIdName);
     // Called after the component has been rendered into the page
-    var url = "http://api.football-data.org/v1/teams/66";
+    var url = "http://api.football-data.org/v1/teams/" + teamIdName;
     Request.get(url)
-    .set('X-Auth-Token', '8921bea73c794f8b848353c45f0eeebd')
-    .then((response) => {
-      console.log('response', response.body.name);
-      this.setState({
-        soccerdata: response.body.name
-      })
-    });
+      .set('X-Auth-Token', '8921bea73c794f8b848353c45f0eeebd')
+      .then((response) => {
+        console.log('response', response.body.name);
+        this.setState({
+          soccerdata: response.body.name,
+          crestUrl: response.body.crestUrl
+        })
+      });
   }
 
   componentWillReceiveProps(nextProps) {
@@ -44,21 +49,21 @@ export default class Team extends React.Component {
     this.search(this.refs.query.value);
   }
 
+
   render() {
-    console.log('TeamName', this.state.soccerdata);
-    return ( <
-      div > {
-        this.props.params.teamId
-      }
-      This is a soccer team <
-      ul >
-      <
-      li > Soccer! < /li> <
-      li > Is! < /li> <
-      li > Fun! < /li> < /
-      ul >
+
+    return (
+      <div>
+      {this.props.params.teamId}
+      This is a soccer team
+      <ul>
       {this.state.soccerdata}
-       </div>
+       <li> Soccer! </li>
+       <li> Is! </li>
+       <li> Fun! </li>
+      </ul>
+      <img src={this.state.crestUrl} />
+      </div>
     )
   }
 }
